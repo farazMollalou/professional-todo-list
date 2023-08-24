44c8a2616c1f4c5ad777f004dc8310487bf86038
farazMollalou <98967259+farazMollalou@users.noreply.github.com>
2023-08-24 12:17:01 +0330

    task actions logic implementation!

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,48 @@
-const task_actions_btns = document.querySelectorAll('.fa-cog')
-const task_actions_btn_box = document.querySelectorAll('.action-btn-box')
+// modals DOM code 
+const modals_list = document.querySelector('.modal-bg').children
+// modals DOM code 
 
-const options_clk_handler = () => {
-    task_actions_btns.forEach((btn, index) => {
-        btn.addEventListener("click", () => {
-            task_actions_btn_box[index].classList.add('show_actions')
-        })
-    })
+let selected_task_id = null
+
+
+function options_clk_handler(event) {
+    let options_box = event.target.nextElementSibling
+    options_box.classList.add("show_actions")
 }
 
-window.addEventListener('click', e => {
-    if (e.target.id === "actions-close-btn") {
-        e.target.parentElement.classList.remove('show_actions')
+function close_options_box(e) {
+    let desired_option_box = e.target.parentElement
+    desired_option_box.classList.remove("show_actions")
+}
+
+function hide_task(e) {
+    let task_elem = e.target.parentElement.parentElement
+    task_elem.classList.add("hide")
+
+    show_remove_confirm_modal(task_elem)
+}
+
+function show_remove_confirm_modal(task_elem) {
+    modals_list[2].classList.add('active')
+    modal_bg.classList.add("active")
+
+    selected_task_id = task_elem.dataset.task_id
+}
+
+function remove_from_storage(id) {
+    let storage_tasks = (JSON.parse(localStorage.getItem("today_list"))).tasks
+    let updated_taskList = storage_tasks.filter(task => task.id != id)
+
+    daily_plan = {
+        tasks: [
+            ...updated_taskList
+        ],
+        global_note: (JSON.parse(localStorage.getItem("today_list"))).global_note
     }
-})
+
+    localStorage.setItem("today_list", JSON.stringify(daily_plan))
+    location.reload()
+}
 
 // local storage coding start
 const task_list_show_field = document.querySelector(".todo-list-wrapper .body .task-list")
@@ -24,16 +53,17 @@ let daily_plan = {
 }
 
 const showTasksOnDOM = tasks => {
+    task_list_show_field.innerHTML = ""
     tasks.forEach(task => {
-        task_list_show_field.insertAdjacentHTML("afterbegin", `<div class="task ${task.priority}">
+        task_list_show_field.insertAdjacentHTML("afterbegin", `<div class="task ${task.priority}" data-task_id="${task.id}">
         <i class="btn fa fa-check"></i>
         <span class="task-title">${task.title}</span>
-        <i class="fa fa-cog" onclick="options_clk_handler()"></i>
+        <i class="fa fa-cog" onclick="options_clk_handler(event)"></i>
         <div class="action-btn-box">
-            <i class="btn fa fa-eraser"></i>
+            <i class="btn fa fa-eraser" onclick="hide_task(event)"></i>
             <i class="btn fa fa-eye"></i>
             <i class="btn fa fa-pen"></i>
-            <i class="btn fa fa-times" id="actions-close-btn"></i>
+            <i class="btn fa fa-times" onclick="close_options_box(event)"></i>
         </div>
     </div>`)
     })
@@ -48,9 +78,6 @@ window.addEventListener('load', () => {
 })
 // local storage coding end
 
-// modals DOM code 
-const modals_list = document.querySelector('.modal-bg').children
-// modals DOM code 
 
 // modal closing code
 const modal_closers_list = document.querySelectorAll(".modal_closer")
@@ -69,11 +96,13 @@ const add_new_task_modal_btn = document.querySelector('#add_new_task_modal_btn')
 const modal_bg = document.querySelector('.modal-bg')
 const new_task_title = document.querySelector(".new-task-form .new-task-input")
 const newTaskAddBtn = document.querySelector(".new-task-form .add-btn")
-const daily_note = document.querySelector(".new-task-form .description-container textarea")
+const task_note = document.querySelector(".new-task-form .description-container textarea")
 
 add_new_task_modal_btn.addEventListener('click', () => {
     modal_bg.classList.add("active")
-    modals_list[0].classList.add('active')
+    setTimeout(() => {
+        modals_list[0].classList.add('active')
+    }, 300)
 })
 
 newTaskAddBtn.addEventListener("click", e => {
@@ -84,14 +113,15 @@ newTaskAddBtn.addEventListener("click", e => {
         title: new_task_title.value,
         isCompleted: false,
         priority: "normal",
+        note: task_note.value,
         id: written_tasks.length
     }
 
     daily_plan = {
-        note: daily_note.value,
         tasks: [
             ...written_tasks, newTask
-        ]
+        ],
+        global_note: ""
     }
 
     localStorage.setItem("today_list", JSON.stringify(daily_plan))
@@ -108,3 +138,8 @@ daily_note_btn.addEventListener("click", () => {
     modals_list[1].classList.add('active')
 })
 
+// confirm remove task start
+
+modals_list[2].children[3].children[0].addEventListener('click', () => remove_from_storage(selected_task_id))
+
+// confirm remove task end
